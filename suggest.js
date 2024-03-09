633249e1820ed1a8533cd9f4c366e2fb6cebc58b
SreyaJoseph <sreyajoseph2004@gmail.com>
2024-03-10 05:10:30 +0530

    generated pass-strong

--- suggest.js
+++ suggest.js
@@ -1,12 +1,12 @@
 function generatePassword() {
-    var likes = document.getElementById("likes").value.toLowerCase();
-    var hobbies = document.getElementById("hobbies").value.toLowerCase();
+
+    var favFood  = document.getElementById("food").value.toLowerCase();
     var favColor = document.getElementById("favColor").value.toLowerCase();
-    var favNumber = document.getElementById("favNumber").value;
+    var favNumber =parseInt(document.getElementById("favNumber").value);
 
     // Generate password suggestion based on user input
-    var password = likes.replace(/\s/g, '') + hobbies.replace(/\s/g, '') + favColor + favNumber;
-    password = password.substring(0, 12); // Limit password length to 12 characters
+    var password =favFood.substring() + favColor.substring() + "@" + favNumber.toString().substring(0, 2);
+    password = password.substring(); // Limit password length to 12 characters
 
     document.getElementById("password").innerHTML = "Password Suggestion: " + password;
 }
